6d8b1e2ba823c779ff1e9d77ad87c8f637cb1fdd
VDigavalli <37352058+VDigavalli@users.noreply.github.com>
2020-05-05 01:07:00 -0400

    Update workerTest.js

--- workerTest.js
+++ workerTest.js
@@ -3,6 +3,6 @@ this.onmessage = function(e){
     console.log("inside worker js");
     
     if(e.data.addThis != undefined) {
-        this.postMessage({result: e.data.addThis.num1 + e.data.addthis.num1});
+        this.postMessage({result: 1 + 5});
     }
 };
